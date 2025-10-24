6189c085fa46789b867314dbe3f70600874eebf0
Gustavo Martinez Reyes <135580374+Reyes15681@users.noreply.github.com>
2025-10-23 21:44:04 -0700

    Added support for create event
    Backend Create Events

--- backend/app.js
+++ backend/app.js
@@ -144,6 +144,35 @@ app.get("/api/checkAuthenticationStatus", authenticateToken, (req, res) => {
   res.status(200).json({ userId: req.user.userId, message: "Authenticated" });
 });
 
+
+app.post("/api/createEvent", authenticateToken, async (req, res) =>
+{
+
+    try{
+        //get inputs from the request
+        const {name, description, startDate, endDate} = req.body;
+        const organizerID = req.user.userId;
+
+        //Connect to the database
+        const connection = await getDbConnection();
+
+        //attempt to insert
+        const [result] = await connection.query( 'INSERT INTO Event (organizerID, name, description, startDate, endDate) VALUES (?, ?, ?, ?, ?)', [organizerID, name, description, startDate, endDate] );
+
+        // respond with event ID
+        const eventID = result.insertId;
+        console.log("Created Event Successfully!");
+        res.status(201).json({eventID: eventID});
+
+
+    } catch (err) { //error handling
+        console.error('Failed to insert: ', err);
+        res.status(500).json({ message: `Server error: ${err.message}` });
+    }
+
+});
+
+
 app.listen(port, () => {
   console.log(`🚀 Listening on port ${port}`);
 });
