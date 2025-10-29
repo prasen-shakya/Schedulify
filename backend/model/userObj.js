6d92079ed40e0062fbcb167065495f211e9f1978
“jdcont” <juliandanielcontreras@gmail.com>
2025-10-29 16:36:47 -0700

    Added these fuck ass models

---
+++ backend/model/userObj.js
@@ -0,0 +1,9 @@
+// module is for the User Class
+class User {
+    constructor(userID, availability) {
+        this.userID = userID;
+        this.availability = availability; // array of availability objects
+    }
+}
+
+export default User;
