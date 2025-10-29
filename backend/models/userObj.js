ae8971b6f1894ab67f149275efa24481e5ee510d
“jdcont” <juliandanielcontreras@gmail.com>
2025-10-29 16:41:59 -0700

    Fixed Prasen's erasing of my models

---
+++ backend/models/userObj.js
@@ -0,0 +1,9 @@
+// module is for the User Class
+class User {
+    constructor(userID, avaialibility) {
+        this.userID = userID;
+        this.availability = avaialibility; // array of availability objects
+    }
+}
+
+export default User;
