0a0154ffa92532db546eec4aca3c000366312e83
Prasen Shakya <pshakya@Prasens-MacBook-Pro.local>
2025-11-04 22:08:11 -0800

    Add overflow on WeeklyCalendar

--- frontend/src/components/event-page/WeeklyCalendar.jsx
+++ frontend/src/components/event-page/WeeklyCalendar.jsx
@@ -1,6 +1,11 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
-export default function WeeklyCalendar({ earliestStartDate, latestEndDate, earliestStartTime, latestEndTime }) {
+export default function WeeklyCalendar({
+  earliestStartDate,
+  latestEndDate,
+  earliestStartTime,
+  latestEndTime,
+}) {
   const [weekdays, setWeekdays] = useState([]);
   const [hours, setHours] = useState([]);
   const [startIndex, setStartIndex] = useState(0);
@@ -17,14 +22,19 @@ export default function WeeklyCalendar({ earliestStartDate, latestEndDate, earli
     const endTime = latestEndTime;
     const times = [];
 
-    for (let d = startDate; d <= endDate; d.setDate(d.getDate() + 1)) {
+    for (
+      let d = new Date(startDate);
+      d <= endDate;
+      d.setDate(d.getDate() + 1)
+    ) {
       days.push(new Date(d));
     }
 
-    const start = parseInt(startTime.split(":")[0], 10);    
+    const start = parseInt(startTime.split(":")[0], 10);
     const end = parseInt(endTime.split(":")[0], 10);
-    for (let d = start; d < end; d++) {
-      times.push(d);
+
+    for (let tempTime = start; tempTime < end; tempTime++) {
+      times.push(tempTime);
     }
 
     setWeekdays(days);
@@ -46,72 +56,102 @@ export default function WeeklyCalendar({ earliestStartDate, latestEndDate, earli
   const currentDays = weekdays.slice(startIndex, startIndex + visibleDays);
 
   return (
-    <div className="pb-5 w-[788px]">     
-      <div
-        className="grid text-xs cally bg-base-100 scale-100 mt-10"
-        style={{ gridTemplateColumns: `auto repeat(${currentDays.length > 5 ? 5 : currentDays.length}, 1fr) auto`}}
-      >
-      <div className="w-[38px] flex justify-center" >
-        <button className="btn btn-ghost btn-sm" onClick={handlePrev} disabled={startIndex == 0}>
-          <svg aria-label="Previous" className="fill-current size-4" slot="previous" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path fill="currentColor" d="M15.75 19.5 8.25 12l7.5-7.5"></path>
-          </svg>
-        </button>
-      </div>
-
-
-      {/* 
-        creating the columns for the calendar 
-        the specific amount of days thats showing in the calendar
-        and the week days with its corresponding date
-      */}  
-      {currentDays.map((day) => (
-          <div
-            key={day.toISOString()}
-            className="font-semibold text-center py-2 border-b border-base-300"
-          >
-            {day.toLocaleDateString("en-US", { weekday: "short", month: "short", day: "numeric" })}
-          </div>
-      ))}
-
-
-      <div className="w-[38px] flex justify-center">
-      <button className="btn btn-ghost btn-sm"            
-              onClick={handleNext} 
-              disabled={startIndex + visibleDays >= weekdays.length}
-      >
-        <svg aria-label="Next" className="fill-current size-4" slot="next" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="m8.25 4.5 7.5 7.5-7.5 7.5"></path></svg>
-      </button>
-      </div>
-      </div>
-
-      {/* 
-        creating the columns for the calendar 
-        including the hours on the left side
-        and the columns on the right side
-      */}  
-      {hours.map((hour) => (
+    <div className="overflow-auto px-1">
+      {console.log("Rendering WeeklyCalendar")}
+      <div className="w-[788px]">
         <div
-          key={hour}
-          className="grid w-[750px] cally text-right text-top"
+          className="bg-base-100 mt-10 grid scale-100 text-[0.7rem]"
           style={{
-          gridTemplateColumns: `auto repeat(${currentDays.length > 5 ? 5 : currentDays.length}, 1fr)`}}
+            gridTemplateColumns: `auto repeat(${currentDays.length > visibleDays ? visibleDays : currentDays.length}, 1fr) auto`,
+          }}
         >
-          <div className="border-r border-base-300 pr-1 w-[38px]">
-            {new Date(0, 0, 0, hour).toLocaleTimeString("en-US", {
-              hour: "numeric",
-              hour12: true,
-            })}
+          <div className="flex w-[38px] justify-center">
+            <button
+              className="btn btn-ghost btn-sm"
+              onClick={handlePrev}
+              disabled={startIndex == 0}
+            >
+              <svg
+                aria-label="Previous"
+                className="size-4 fill-current"
+                slot="previous"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M15.75 19.5 8.25 12l7.5-7.5"
+                ></path>
+              </svg>
+            </button>
           </div>
 
+          {/* 
+            creating the columns for the calendar 
+            the specific amount of days thats showing in the calendar
+            and the week days with its corresponding date
+          */}
           {currentDays.map((day) => (
             <div
-              key={`${day.toISOString()}-${hour}`}
-              className="border-r border-b border-base-200 h-[30px] hover:bg-base-200 cursor-pointer"
-            ></div>
+              key={day.toISOString()}
+              className="border-base-300 border-b py-2 text-center font-semibold"
+            >
+              {day.toLocaleDateString("en-US", {
+                weekday: "short",
+                month: "short",
+                day: "numeric",
+              })}
+            </div>
           ))}
+
+          <div className="flex w-[38px] justify-center">
+            <button
+              className="btn btn-ghost btn-sm"
+              onClick={handleNext}
+              disabled={startIndex + visibleDays >= weekdays.length}
+            >
+              <svg
+                aria-label="Next"
+                className="size-4 fill-current"
+                slot="next"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path fill="currentColor" d="m8.25 4.5 7.5 7.5-7.5 7.5"></path>
+              </svg>
+            </button>
+          </div>
         </div>
-      ))}
+
+        {/* 
+          creating the columns for the calendar 
+          including the hours on the left side
+          and the columns on the right side
+        */}
+        {hours.map((hour) => (
+          <div
+            key={hour}
+            className="text-top grid w-[750px] text-right text-[0.7rem]"
+            style={{
+              gridTemplateColumns: `auto repeat(${currentDays.length > visibleDays ? visibleDays : currentDays.length}, 1fr)`,
+            }}
+          >
+            <div className="border-base-300 w-[38px] border-r pr-1">
+              {new Date(0, 0, 0, hour).toLocaleTimeString("en-US", {
+                hour: "numeric",
+                hour12: true,
+              })}
+            </div>
+
+            {currentDays.map((day) => (
+              <div
+                key={`${day.toISOString()}-${hour}`}
+                className="border-base-200 h-[30px] cursor-pointer border-r border-b"
+              ></div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
