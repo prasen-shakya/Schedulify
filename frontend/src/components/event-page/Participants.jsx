a5a19e909917882a287865d53b3a161be98e4eeb
prasen-shakya <prasen1265@gmail.com>
2025-11-10 20:59:13 -0800

    Show participant highlight on hover

--- frontend/src/components/event-page/Participants.jsx
+++ frontend/src/components/event-page/Participants.jsx
@@ -1,19 +1,33 @@
-const Participants = ({ participants }) => {
+const Participants = ({ participants, highlightedParticipant }) => {
   return (
     <div>
       <p className="mb-2">Event Participants</p>
       {participants && participants.length > 0 ? (
         <div className="flex flex-col gap-2">
           {participants.map((participant) => {
-            const { UserId: id, Name: name } = participant;
+            const { UserID: id, Name: name } = participant;
+            const isHighlighted =
+              highlightedParticipant && highlightedParticipant.includes(id);
+            const isDimmed =
+              highlightedParticipant && !highlightedParticipant.includes(id);
+
             return (
-              <div key={`${name}${id}`} className="flex items-center gap-2">
+              <div
+                key={`${name}${id}`}
+                className={`flex items-center gap-2 transition-all ${
+                  isHighlighted ? "my-1 scale-105" : ""
+                }`}
+              >
                 <div className="avatar avatar-placeholder">
-                  <div className="bg-primary text-neutral-content w-8 rounded-full">
+                  <div
+                    className={`w-8 rounded-full ${isHighlighted ? "ring-primary ring-2 ring-offset-1" : ""} ${isDimmed ? "bg-gray-300 text-gray-500" : "bg-primary text-neutral-content"}`}
+                  >
                     {name.charAt(0).toUpperCase()}
                   </div>
                 </div>
-                <div className="text-sm">
+                <div
+                  className={`text-sm ${isHighlighted ? "text-primary" : ""} ${isDimmed ? "text-gray-400 line-through" : ""}`}
+                >
                   {name.charAt(0).toUpperCase() + name.slice(1)}
                 </div>
               </div>
