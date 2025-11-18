e1dbf580fca2290e27620ff2634e179bd06e619c
Adam <adamrburns@outlook.com>
2025-11-17 17:17:25 -0800

    Added how to use and github link

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,28 @@
 import { useAuth } from "@/contexts/AuthContext";
 import calendarLogo from "@/images/calendar.png";
+//import { useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
   const { isAuthenticated, showAuthenticationModal, signOut } = useAuth();
+  //const location = useLocation();
+  //const path = location.pathname; 
   return (
-    <div className="navbar bg-base-100 sticky top-0 left-0 w-full shadow-sm">
+    <div className="navbar bg-base-100 sticky top-0 z-50 left-0 w-full shadow-sm">
       <div className="mx-8 flex w-full justify-between">
         <div className="flex items-center gap-2">
           <img className="h-[25px]" src={calendarLogo} alt="Schedulify Logo" />
           <a href="/" className="text-xl font-semibold">
             Schedulify
           </a>
+        </div> 
+        
+        <div className="flex items-center gap-4"> {/* RIGHT ALIGN BUTTONS */}
+        <div>
+        <a href="/#how"><button className="btn btn-ghost mt-2 h-10">
+            
+          How to Use
+        </button></a>
+        
         </div>
         <button
           className="btn btn-primary mt-2 h-10"
@@ -18,6 +30,7 @@ export const Navbar = () => {
         >
           {isAuthenticated ? "Sign Out" : "Sign In"}
         </button>
+        </div>
       </div>
     </div>
   );
